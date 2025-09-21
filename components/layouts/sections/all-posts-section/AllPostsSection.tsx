894b8d907a1b96d53e4787abfd7d759de7a239a1
BorofskyDev <joelborofsky@mac.com>
2025-09-20 21:59:02 -0500

    refactor: update import path for PostData in AllPostsSection component

--- components/layouts/sections/all-posts-section/AllPostsSection.tsx
+++ components/layouts/sections/all-posts-section/AllPostsSection.tsx
@@ -4,7 +4,7 @@
 import { useState } from 'react'
 import usePaginatedPosts from '@/lib/hooks/usePaginatedPosts'
 import { SmallPostCard } from '@/components/ui/cards/'
-import { PostData } from '@/components/ui/cards/posts/post-card/PostCard'
+import { PostData } from '@/components/ui/cards/'
 import { RecentPost } from '@/lib/functions/getRecentPosts'
 import { GeneralSection } from '@/components/layouts/'
 import { Heading } from '@/components/ui/common'

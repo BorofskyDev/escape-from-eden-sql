8cd83f5f24d39f23e13b9e48173716d1a97d11bb
BorofskyDev <joelborofsky@mac.com>
2025-09-20 22:07:14 -0500

    refactor: update import statement for FormField in TipCard component

--- components/ui/cards/TipCard.tsx
+++ components/ui/cards/TipCard.tsx
@@ -1,7 +1,7 @@
 // components/ui/cards/TipCard.tsx
 'use client'
 import { useState } from 'react'
-import FormField from '../common/inputs/form-field/FormField'
+import { FormField } from '../common'
 
 export default function TipCard() {
   const [amount, setAmount] = useState('')
